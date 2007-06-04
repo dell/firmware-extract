7a022fb71c069b037db682d371e079fbbb9194fb
Michael E Brown <michael_e_brown@dell.com>
2007-06-04 00:48:11 -0500

    add multiprocess extraction. huge speedup.

--- pymod/ini_thread_proxy.py
+++ pymod/ini_thread_proxy.py
@@ -6,15 +6,15 @@ import SimpleXMLRPCServer
 import threading
 
 class XmlRpcThread(threading.Thread):
-    def __init__ (self,threadNumber, port, iniPath):
+    def __init__ (self, threadNumber, port, ini, iniPath):
         threading.Thread.__init__(self)
         self.stop = 0
         self.id = threadNumber
         self.iniPath = iniPath 
         self.port = port 
         self.lock = threading.Lock()
-        self.ini = ConfigParser.ConfigParser()
-        self.ini.read( self.iniPath )
+        self.ini = ini
+        self.running=0
         self.start()
 
     def sync(self):
@@ -24,15 +24,20 @@ class XmlRpcThread(threading.Thread):
             self.ini.write(fh)
             fh.close()
         except:
-            self.lock.release()
             import traceback
             traceback.print_exc()
 
+        self.lock.release()
         return 0
 
     def kill(self):
-        self.stop=0 
+        self.stop=1 
+        self.running=0
         return 1
+
+    def ping(self, value):
+        print "was pinged."
+        return value
 
     def _dispatch(self, method, params):
         if hasattr(self, method) and method != "run" and not method.startswith("_"):
@@ -46,6 +51,7 @@ class XmlRpcThread(threading.Thread):
     def run(self):
         server = SimpleXMLRPCServer.SimpleXMLRPCServer(('127.0.0.1', self.port), logRequests=0)
         server.register_instance(self)
+        self.running=1
 
         while not self.stop:
             server.handle_request()
