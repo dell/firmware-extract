e4335f223c328ab9c94a4db3cb83cd75336f584e
Michael E Brown <michael_e_brown@dell.com>
2008-01-20 17:00:40 -0600

    start implementing extract... wip.

--- ft-cli-plugin/extract.py
+++ ft-cli-plugin/extract.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-# Copyright 2006 Duke University 
+# Copyright 2006 Duke University
 # Written by Seth Vidal
 
 """
@@ -20,12 +20,15 @@ firmwaretool plugin
 """
 
 import os
+import sqlobject
+import stat
 
 from firmwaretools.trace_decorator import decorate, traceLog, getLog
 import firmwaretools.plugins as plugins
 
 import ftcommands
 import dell_repo_tools.extract_db as extract_db
+import firmwaretools.pycompat as pycompat
 
 plugin_type = (plugins.TYPE_CLI,)
 requires_api_version = "2.0"
@@ -45,11 +48,9 @@ def config_hook(conduit, *args, **kargs):
 
 def checkConf(conf):
     if getattr(conf, "extract_topdir", None) is None:
-        print "foo"
         import firmwaretools
         conf.extract_topdir = os.path.join(firmwaretools.DATADIR, "firmware", "extract")
     if getattr(conf, "db_path", None) is None:
-        print "bar"
         import firmwaretools
         conf.db_path = os.path.join(firmwaretools.DATADIR, "firmware", "extract.db")
     return conf
@@ -67,8 +68,6 @@ class ExtractCommand(ftcommands.YumCommand):
 
     decorate(traceLog())
     def initDb(self):
-        conf.db_path
-        import sqlobject
         if os.path.exists(conf.db_path):
             moduleLogVerbose.info("unlinking old db: %s" % conf.db_path)
             os.unlink(conf.db_path)
@@ -76,6 +75,7 @@ class ExtractCommand(ftcommands.YumCommand):
         if not os.path.exists(os.path.dirname(conf.db_path)):
             os.makedirs(os.path.dirname(conf.db_path))
 
+        moduleLogVerbose.info("Initializing db at %s" % conf.db_path)
         sqlobject.sqlhub.processConnection = sqlobject.connectionForURI(
                 'sqlite://%s' % conf.db_path)
         extract_db.createTables()
@@ -86,12 +86,55 @@ class ExtractCommand(ftcommands.YumCommand):
         if base.opts.initdb:
             self.initDb()
 
-        print "HEY THERE: %s" % conf.extract_topdir
-        print "HEY THERE: %s" % conf.db_path
-        print "extract: %s" % processedArgs
+        for file in walkFiles(processedArgs):
+            moduleLogVerbose.info("Processing %s" % file)
+            if not os.path.exists(file):
+                moduleLogVerbose.critical("File does not exist: %s" % file)
+                continue
 
-        # algorithm
-        #  done if already processed
+            # check already processed
+            status = "unprocessed"
+            module = None
+            moduleVersion = None
+            if not alreadyProcessed(file):
+                # extract()
+                # add to db
+                addFile(file, status, module, moduleVersion)
 
         return [0, "Done"]
+
+decorate(traceLog())
+def alreadyProcessed(file):
+    name, size, ctime, md5sum = fileDetails(file)
+    if extract_db.ProcessedFile.select(extract_db.ProcessedFile.name == name).count() > 0:
+        return True
+    return False
+
+decorate(traceLog())
+def addFile(file, status, module, moduleVersion):
+    name, size, ctime, md5sum = fileDetails(file)
+    extract_db.ProcessedFile(
+        status=status, name=name, size=size,
+        ctime=ctime, md5sum=md5sum, module=module, moduleVersion=moduleVersion)
+
+decorate(traceLog())
+def fileDetails(file):
+    name = os.path.basename(file)
+    f_stat = os.stat(file)
+    size = f_stat[stat.ST_SIZE]
+    ctime = f_stat[stat.ST_CTIME]
+    md5sum = ""
+    return (name, size, ctime, md5sum)
+
+decorate(traceLog())
+def walkFiles(paths):
+    for path in paths:
+        if os.path.isdir(path):
+            for topdir, dirlist, filelist in pycompat.walkPath(os.path.realpath(path)):
+                filelist.sort()
+                for file in filelist:
+                    yield file
+        else:
+            yield path
+
 
